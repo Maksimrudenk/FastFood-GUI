34740b957c6e0d81be3d53fd700d1e5ee4dd457f
Maksim <MaksimRudenkoSc50@gmail.com>
2024-05-01 12:07:30 +0300

    first commit

--- src/main/java/com/gui/Main.java
+++ src/main/java/com/gui/Main.java
@@ -1,2 +1,15 @@
-package com.gui;public class Main {
+package com.gui;
+
+import javax.swing.*;
+
+public class Main {
+    private JPanel main;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Main");
+        frame.setContentPane(new Main().main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
